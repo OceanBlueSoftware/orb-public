aced8688b05443fb6cd453c47e1415c24e2ced62
fredzhu-obs <tong.zhu@oceanbluesoftware.co.uk>
2025-08-31 17:36:30 +0100

    FREE-248 Add check for port and endpoint for the capabilities returned from LiveApp to avoid Json lib crash

--- orblibrary/moderator/Moderator.cpp
+++ orblibrary/moderator/Moderator.cpp
@@ -186,16 +186,26 @@ bool Moderator::startWebSocketServer() {
     }
 
     // Get the endpoint and port from the capabilities
-    std::string endpoint = JsonUtil::getStringValue(capabilities["result"], "jsonRpcServerEndpoint");
-    int port = JsonUtil::getIntegerValue(capabilities["result"], "jsonRpcServerPort");
-
-    LOGI("Create and start WebSocket Server - endpoint: " << endpoint << ", port: " << port);
-    // Create and start the WebSocket Server
-    std::unique_ptr<orb::networkServices::JsonRpcService::ISessionCallback> callback =
-        std::make_unique<JsonRpcCallback>();
-    mWebSocketServer = std::make_shared<orb::networkServices::JsonRpcService>(port,endpoint,std::move(callback));
-    mWebSocketServer->SetOpAppEnabled(mAppType == orb::APP_TYPE_OPAPP);
-    return mWebSocketServer->Start();
+    Json::Value result = capabilities["result"];
+    const std::string SERVER_ENDPOINT_KEY = "jsonRpcServerEndpoint";
+    const std::string SERVER_PORT_KEY = "jsonRpcServerPort";
+    if (JsonUtil::HasParam(result, SERVER_ENDPOINT_KEY, Json::stringValue) &&
+        JsonUtil::HasParam(result, SERVER_PORT_KEY, Json::intValue))
+    {
+        std::string endpoint = JsonUtil::getStringValue(result, SERVER_ENDPOINT_KEY);
+        int port = JsonUtil::getIntegerValue(result, SERVER_PORT_KEY);
+        LOGI("Create and start WebSocket Server - endpoint: " << endpoint << ", port: " << port);
+        // Create and start the WebSocket Server
+        std::unique_ptr<orb::networkServices::JsonRpcService::ISessionCallback> callback =
+            std::make_unique<JsonRpcCallback>(mVideoWindow);
+        mWebSocketServer = std::make_shared<orb::networkServices::JsonRpcService>(port,endpoint,std::move(callback));
+        mWebSocketServer->SetOpAppEnabled(mAppType == orb::APP_TYPE_OPAPP);
+        // set WebSocketService to VideoWindow
+        mVideoWindow->setWebSocketService(mWebSocketServer);
+        return mWebSocketServer->Start();
+    }
+    LOGE("Websocket Server can not start as Capabilities response does not contain jsonRpcServerEndpoint or jsonRpcServerPort");
+    return false;
 }
 
 } // namespace orb
